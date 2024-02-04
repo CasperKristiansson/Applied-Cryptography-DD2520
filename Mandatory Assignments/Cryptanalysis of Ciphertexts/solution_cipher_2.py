02306733a815327c3aacb55621750145ed548e80
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-02-04 19:24:48 +0100

    Cipher 2 update frequencies

--- Mandatory Assignments/Cryptanalysis of Ciphertexts/solution_cipher_2.py
+++ Mandatory Assignments/Cryptanalysis of Ciphertexts/solution_cipher_2.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import numpy as np
 
-with open(r'Mandatory Assignments\Cryptanalysis of Ciphertexts\cipher_2.txt') as f:
+with open(r'Mandatory Assignments/Cryptanalysis of Ciphertexts/cipher_2.txt') as f:
     cipher_2 = f.read()
 
 cipher_2 = cipher_2.replace("\n", "")
@@ -11,6 +11,12 @@ english_letter_freq = {
     'E': 12.7, 'T': 9.1, 'A': 8.2, 'O': 7.5, 'I': 7.0, 'N': 6.7, 'S': 6.3, 'H': 6.1, 'R': 6.0,
     'D': 4.3, 'L': 4.0, 'C': 2.8, 'U': 2.8, 'M': 2.4, 'W': 2.4, 'F': 2.2, 'G': 2, 'Y': 2.0,
     'P': 1.9, 'B': 1.5, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.074
+}
+
+english_letter_freq = {
+    "A": 0.072, "B": 0.013, "C": 0.024, "D": 0.037, "E": 0.112, "F": 0.020, "G": 0.018, "H": 0.054, "I": 0.061,
+    "J": 0.001, "K": 0.007, "L": 0.035, "M": 0.021, "N": 0.059, "O": 0.066, "P": 0.017, "Q": 0.001, "R": 0.053,
+    "S": 0.056, "T": 0.080, "U": 0.024, "V": 0.009, "W": 0.021, "X": 0.001, "Y": 0.017, "Z": 0.001, "_": 0.120,
 }
 
 
@@ -88,8 +94,8 @@ def interleave_columns(columns):
 ics_by_length = {l: calculate_frequencies_for_key_length(cipher_2, l) for l in range(1, max_key_length + 1)}
 
 # Step 2 - Find the key length with the highest IC
-total_freq = sum(english_letter_freq.values())
-english_letter_freq = {char: freq / total_freq for char, freq in english_letter_freq.items()}
+# total_freq = sum(english_letter_freq.values())
+# english_letter_freq = {char: freq / total_freq for char, freq in english_letter_freq.items()}
 
 # Step 3 - Find the best shift for each column
 key_length = 12
@@ -100,4 +106,4 @@ best_shifts = [find_best_shift_for_column(column) for column in columns]
 decoded_columns = apply_shifts_to_columns(columns, best_shifts)
 decoded_text = interleave_columns(decoded_columns)
 decoded_text = decoded_text.replace("_", " ")
-print(decoded_text[:500])
+print(decoded_text[:1000])
